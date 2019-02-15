8e4131917f111cfb9dc4068d9ed5c450093e0a80
Aszmel <aszmel@o2.pl>
2019-02-15 21:38:25 +0100

    first commit

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../App";
+
+it("shows a comment box", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  expect(div.innerHTML).toContain("Comment Box");
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("shows a comment list", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  expect(div.innerHTML).toContain("Comment List");
+  ReactDOM.unmountComponentAtNode(div);
+});
